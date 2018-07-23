886bf0afe02305e8ca15bcedefe8805755a0ae62
andgrand <grddron@gmail.com>
2018-07-23 17:20:12 +0300

    vuex page, limit, sort

--- app/store/modules/smart-table.js
+++ app/store/modules/smart-table.js
@@ -1,8 +1,18 @@
 // import {LOAD_ROWS, LOADED_ROWS} from '~store/types'
 import Structure from '~js/modules/structure'
+
+let defaultQuery = {
+  sort: {createdAt: 1},
+  where: {},
+  limit: 10,
+  page: 1
+};
+
 export default {
   state: {
     count: 0,
+    query: clone(defaultQuery),
+    defaultQuery,
     rows: [],
     info: {},
     loading: 0,
@@ -28,18 +38,35 @@ export default {
     },
     ['SET_INFO'](state, info) {
       state.info = info;
-    }
+    },
+    ['UPDATE_QUERY'](state, query) {
+      state.query = query;
+    },
+    ['UPDATE_DEFAULT_QUERY'](state, defaultQuery) {
+      state.defaultQuery = clone(defaultQuery);
+    },
+  },
+  getters: {
+    sortBy(state) {
+      return Object.keys(state.query.sort)[0];
+    },
+    sortDirection(state) {
+      return Object.values(state.query.sort)[0];
+    },
   },
   actions: {
-    async reloadRows({commit, state}, options) {
+    async reloadRows({commit, state}, query) {
+      commit('UPDATE_QUERY', query);
       commit('RELOAD_DATA');
-      commit('SET_ROWS', await state.model.all(options));
+      commit('SET_ROWS', await state.model.all(state.query));
       commit('DATA_READY');
     },
-    async loadData({commit, state}, {rowsOptions, infoLoader, dataFetched, struct}) {
+    async loadData({commit, state}, {query, infoLoader, dataFetched, struct}) {
+      commit('UPDATE_DEFAULT_QUERY', query);
+      commit('UPDATE_QUERY', query);
       commit('LOAD_DATA');
       let data = await Promise.allObject({
-        rows: await state.model.all(options),
+        rows: await state.model.all(state.query),
         info: await infoLoader(state),
       });
 
@@ -50,6 +77,19 @@ export default {
       commit('SET_ROWS', data.rows);
       commit('SET_INFO', data.info);
       commit('DATA_READY');
-    }
+    },
+    async setPage({state, dispatch}, page) {
+      dispatch('reloadRows', {...state.query, page});
+    },
+    async setSort({state, dispatch}, {sortBy, sortDirection}) {
+      let sort = {[sortBy]: sortDirection};
+      let page = state.defaultQuery.page;
+      let where = state.defaultQuery.where;
+      dispatch('reloadRows', {...state.query, ...{sort, page, where}});
+    },
+    async setLimit({state, dispatch}, limit) {
+      let page = state.defaultQuery.page;
+      dispatch('reloadRows', {...state.query, ...{limit, page}});
+    },
   }
 };
